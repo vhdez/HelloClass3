57e1c6a05e9082e9e8c5ec7c78da6deeab09e3ac
Mr. Hernandez <mrhernandez@hernandewings-iMac.local>
2020-11-25 10:21:52 -0500

    Initial commit of HelloClass3 for my class to modify.

---
+++ src/edu/sla/mrhernandez/Main.java
@@ -0,0 +1,9 @@
+package edu.sla.mrhernandez;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hi!  It's Mr. Hernandez.  Who's here?");
+    }
+}
