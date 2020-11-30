7847ac1862c374b0fdc3d2585ca2cb6ed357a46b
holaroman <aroman22@scienceleadership.org>
2020-11-30 13:55:44 -0500

    it's me aidan

--- src/edu/sla/mrhernandez/Main.java
+++ src/edu/sla/mrhernandez/Main.java
@@ -9,6 +9,7 @@ public class Main {
         System.out.println("Hello, Matthew present ^.^");
         System.out.println("Hey! It's Cia ^^");
         System.out.println("Mr. Hernandez is still here.");
-        System.out.println("Hello, this is Ethan Chen");
+        System.out.println("..hacking.... I'm in- Aidan");
+
     }
 }
