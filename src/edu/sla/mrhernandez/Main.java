d6bfb5c76541ed0d1fb0bd020f4eaa8f7e1bad61
bobermcboberson <tf2spyman5@gmail.com>
2020-11-30 13:18:52 -0500

    Alonso was here

--- src/edu/sla/mrhernandez/Main.java
+++ src/edu/sla/mrhernandez/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         System.out.println("Hi!  It's Mr. Hernandez.  Who's here?");
+        System.out.println("Hi!  It's Alonso! Who else is here?");
     }
 }
